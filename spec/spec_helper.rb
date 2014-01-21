28c94551b2c918e50f5e9381f6c660d99e40cbd3
Zach Rowe <zach@alum.rpi.edu>
2014-01-21 16:43:42 -0500

    modify spec helper to load dummy app
    also remove time cop and Database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,49 +1,9 @@
 require 'rubygems'
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
 
-  config.mock_with :rspec
-
-  config.before(:each) do
-    if example.metadata[:js]
-      DatabaseCleaner.strategy = :truncation, {:except => %w[roles states settings content_types]}
-    elsif example.metadata[:search]
-      DatabaseCleaner.strategy = :truncation, {:except => %w[roles states settings content_types]}
-      ThinkingSphinx::Test.init
-      ThinkingSphinx::Test.start_with_autostop
-    else
-      DatabaseCleaner.strategy = :transaction
-    end
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-  config.alias_it_should_behave_like_to :it_has_behavior,         'has behavior:'
-  config.alias_it_should_behave_like_to :it_should_have_attached, 'it has attached'
-
-   config.before(:suite) do
-    set_theme if respond_to? :set_theme
-  end
-  config.before(:each) do
-    Timecop.return
-    #FakeWeb.clean_registry
-
-    # don't keep an audit log for normal tests
-    PaperTrail.enabled = false
-  end
-
-  config.before(:each, :audit => true) do
-    PaperTrail.enabled = true
-  end
+  config.include FactoryGirl::Syntax::Methods
 end
-# end
-
-# Spork.each_run do
-# This code will be run each time you run your specs.
-
-# end
