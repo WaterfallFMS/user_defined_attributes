0052a5073b3492cf5a0a7f76718e323bb4f1939f
Zach Rowe <zach@alum.rpi.edu>
2014-01-27 17:50:07 -0500

    add database cleaner to make tests idempotent

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source "https://rubygems.org"
 # development dependencies will be added by default to the :development group.
 gemspec
 gem 'factory_girl_rails', '~> 4.0'
+gem "database_cleaner", "~> 1.2.0"
 
 # Declare any dependencies that are still in development here instead of in
 # your gemspec. These might include edge Rails or gems from your path or
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,20 @@
 require 'rubygems'
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
+require 'database_cleaner'
 require 'factories/leads'
 
 RSpec.configure do |config|
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   config.include FactoryGirl::Syntax::Methods
 end
