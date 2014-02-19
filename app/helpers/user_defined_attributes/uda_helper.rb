0295a10fe57db08a09b7d0f5affe149fc3cd5841
John Kamenik <jkamenik@gmail.com>
2014-02-19 13:43:30 -0500

    removing extra line

--- app/helpers/user_defined_attributes/uda_helper.rb
+++ app/helpers/user_defined_attributes/uda_helper.rb
@@ -11,7 +11,6 @@ module UserDefinedAttributes
     def render_uda_in_form(form, build_type = nil)
       build_type ||= UserDefinedAttributes::Config.form_builder
       
-      
       render partial: "user_defined_attributes/form_for_#{build_type.to_s}", locals: {f: form}
     end
 
